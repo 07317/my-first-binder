fde0edb58244ed7d6db055ad289b8aa84b6df8ea
07317 <123767534+07317@users.noreply.github.com>
2023-01-29 16:42:18 -0600

    pandas and tensorflow

--- testsample.py
+++ testsample.py
@@ -1,6 +1,3 @@
-import pandas as pd
-#import csv                    
-#import numpy as np                     
-#import matplotlib.pyplot as plt        
-#import tensorflow as tf                
-#from tensorflow import keras
+import pandas as pd     
+import tensorflow as tf                
+from tensorflow import keras
