70557f725a11f0d2f95e2ae97419ccc08a489085
07317 <123767534+07317@users.noreply.github.com>
2023-01-29 16:24:23 -0600

    committing with pandas

--- testsample.py
+++ testsample.py
@@ -2,5 +2,5 @@ import pandas as pd
 #import csv                    
 #import numpy as np                     
 #import matplotlib.pyplot as plt        
-import tensorflow as tf                
-from tensorflow import keras
+#import tensorflow as tf                
+#from tensorflow import keras
